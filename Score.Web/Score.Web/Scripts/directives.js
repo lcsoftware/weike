f8312db1ccbc65db3ec66440c740c15143dad03b
nevupo <lcsoftware@126.com>
2014-10-15 13:58:07 +0800

    auto commited

--- Score.Web/Score.Web/Scripts/directives.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-var aDirective = angular.module('app.directives', []);
-
-aDirective.directive('appVersion', ['version', function (version) {
-    return function (scope, elm, attrs) {
-        elm.text(version);
-    };
-}]);
--- Score.Web/Score.Web/Scripts/filters.js
+++
@@ -1,9 +0,0 @@
-﻿'use strict';
-
-var aFilter = angular.module('app.filters', []);
-
-aFilter.filter('interpolate', ['version', function (version) {
-    return function (text) {
-        return String(text).replace(/\%VERSION\%/mg, version);
-    }
-}]);
