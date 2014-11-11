2b553a44770b15434b9f3d86050e879a5e67887d
nevupo <lcsoftware@126.com>
2014-11-11 13:31:51 +0800

    auto commited

--- Web.Score/Web.Score/DataProvider/UploadHandler.ashx.cs
+++ Web.Score/Web.Score/DataProvider/UploadHandler.ashx.cs
@@ -13,50 +13,24 @@ namespace App.Web.Score.DataProvider
         private string _UploaderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Uploads");
         public void ProcessRequest(HttpContext context)
         {
-            string newFileName = Guid.NewGuid().ToString().Replace("-", "");
-            int index = context.Request.Files[0].FileName.LastIndexOf('.');
-            if (index == -1) { index = 0; }
-            newFileName += context.Request.Files[0].FileName.Substring(index);
-            context.Response.Write(newFileName);
-            context.Request.Files[0].SaveAs(System.IO.Path.Combine(_UploaderPath, newFileName));
+            try
+            {
+                //string newFileName = Guid.NewGuid().ToString().Replace("-", "");
+                //int index = context.Request.Files[0].FileName.LastIndexOf('.');
+                //if (index == -1) { index = 0; }
+                //newFileName += context.Request.Files[0].FileName.Substring(index);
+                //context.Request.Files[0].SaveAs(System.IO.Path.Combine(_UploaderPath, newFileName));
+                //context.Response.Write(newFileName);
 
-            //context.Response.ContentType = "text/plain";
-            //try
-            //{
-            //    int id = Convert.ToInt32(context.Request["id"]);
-            //    int type = Convert.ToInt32(context.Request["type"]);
-            //    string title = context.Request["title"];
-            //    string remark = context.Request["remark"];
-            //    int belong = Convert.ToInt32(context.Request["belong"]);
-            //    string filePath = ConfigurationManager.AppSettings["FileUpload"].ToString() + Enum.GetName(typeof(Category), type);
-            //    string a = Enum.GetName(typeof(Category), type);
+                ReadFromExcel(@"f:\fff.xls");
 
-            //    int index = context.Request.Files[0].FileName.LastIndexOf('.');
-            //    if (index == -1) { index = 0; }
-            //    string fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff") + context.Request.Files[0].FileName.Substring(index);
 
-            //    string originalName = context.Request.Files[0].FileName;
-            //    if (context.Request.Files[0].ContentLength > 0)
-            //    {
-            //        if (!Directory.Exists(filePath))
-            //        {
-            //            Directory.CreateDirectory(filePath);
-            //        }
-            //        context.Request.Files[0].SaveAs(filePath + "\\" + fileName);
 
-            //        FileInsert(fileName, type, belong, title, remark, originalName);
-
-            //        context.Response.Write("上传成功!");
-            //    }
-            //    else
-            //    {
-            //        context.Response.Write("请选择上传文件!");
-            //    }
-            //}
-            //catch
-            //{
-            //    context.Response.Write("上传失败!");
-            //}
+            }
+            catch (Exception ex)
+            {
+                context.Response.Write("-1");
+            }
         }
 
         public bool IsReusable
@@ -72,11 +46,12 @@ namespace App.Web.Score.DataProvider
             int index = fileName.LastIndexOf('.');
             if (index == -1) { index = 0; }
             string ext = fileName.Substring(index);
-            string connStr = "";
-            if (ext == ".xls")
-                connStr = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + fileName + ";" + ";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1\"";
-            else
-                connStr = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=" + fileName + ";" + ";Extended Properties=\"Excel 12.0;HDR=YES;IMEX=1\"";
+            //string connStr = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};HDR=YES;IMEX=1;Extended Properties=\"{1}\"", fileName, ext == ".xls" ? "8.0" : "12.0");
+            string connStr = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Persist Security Info=True;Extended Properties=\"{1}\";", fileName, ext == ".xls" ? "8.0" : "12.0");
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(connStr);
+            conn.Open();
+            System.Data.DataTable dtSheetName = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            return dtSheetName;
         }
     }
 }
