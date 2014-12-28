08498965f6b733b34bdc77d7e999ce951e4eeba0
shujianhua0 <405045115@qq.com>
2014-12-28 16:08:28 +0800

    auto commited

--- IES/IES2/Resource/Scripts/Resource/ResourceControllers.js
+++ IES/IES2/Resource/Scripts/Resource/ResourceControllers.js
@@ -4,5 +4,11 @@ var appResource = angular.module('app.resource.controllers', []);
 
 appResource.controller('ResourceCtrl', ['$scope', function ($scope) {
     $scope.$emit('onActived', 1);
+
+    $scope.actived = 2;
+
+    $scope.$on("onActived", function (event, active) {
+        $scope.actived = active;
+    });
 }]);
 
