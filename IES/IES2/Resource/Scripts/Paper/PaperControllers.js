c5852087c9d814c9451e8610cde3926864112e48
nevupo <lcsoftware@126.com>
2014-12-30 01:13:07 +0800

    auto commited

--- IES/IES2/Resource/Scripts/Paper/PaperControllers.js
+++ IES/IES2/Resource/Scripts/Paper/PaperControllers.js
@@ -5,9 +5,33 @@ var appPaper = angular.module('app.paper.controllers', ['app.paper.services']);
 appPaper.controller('PaperListCtrl', ['$scope', 'PaperService', function ($scope, PaperService) {
     $scope.$emit('onActived', 2);
 
+    $scope.keyword = '';
+
     $scope.paperTypes = [];
     $scope.conditionPaperTypes = [];
 
+    $scope.typeSelection = -1;
+    $scope.createrSelection = -1; 
+
+    $scope.$watch('typeSelection', function (newValue) {
+        find(newValue, $scope.createrSelection);
+    });
+
+    $scope.$watch('createrSelection', function (newValue) {
+        find($scope.typeSelection, newValue);
+    });
+
+    $scope.typeChanged = function (v) {
+        $scope.typeSelection = v;
+    }
+
+    $scope.createrChanged = function (v) {
+        $scope.createrSelection = v;
+    }
+
+    var find = function (typeSelection, createrSelection) { 
+        console.log(typeSelection, createrSelection);
+    }
     ///初始化试卷类型
     PaperService.getPaperTypes(function (data) {
         $scope.paperTypes = data;
