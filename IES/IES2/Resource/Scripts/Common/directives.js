53688aed700c8086aa464979232b9e2e3a0b7c6d
nevupo <lcsoftware@126.com>
2015-01-09 23:54:04 +0800

    auto commited

--- IES/IES2/Resource/Scripts/Common/directives.js
+++ IES/IES2/Resource/Scripts/Common/directives.js
@@ -1,9 +1,22 @@
 ﻿'use strict';
 
-angular.module('app.directives', [])
+var directiveApp = angular.module('app.directives', []);
 
-    .directive('appVersion', ['version', function (version) {
-        return function (scope, elm, attrs) {
-            elm.text(version);
-        };
-    }]);
+directiveApp.directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
+        elm.text(version);
+    };
+}]);
+
+directiveApp.directive('onFinishRenderFilters', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                });
+            }
+        }
+    };
+});
